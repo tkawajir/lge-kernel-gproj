ec84fee2bd4b7777e6d09b32721369912638e076
tkawajir <taka.kawajiri@gmail.com>
2014-12-22 13:59:02 +0900

    Revert "gv: Add Support VDD Contorol/CPU Overclock/CPU Underclock/GPU Overclock"
    This reverts commit 5a6c553461f688ca787ada6fb39db6d928cbeed8.

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -336,13 +336,13 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 
 	if (cpufreq_frequency_table_cpuinfo(policy, table)) {
 #ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
-		policy->cpuinfo.min_freq = 135000;
-		policy->cpuinfo.max_freq = 1836000;
+		policy->cpuinfo.min_freq = CONFIG_MSM_CPU_FREQ_MIN;
+		policy->cpuinfo.max_freq = CONFIG_MSM_CPU_FREQ_MAX;
 #endif
 	}
 #ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
-	policy->min = 135000;
-	policy->max = 1836000;
+	policy->min = CONFIG_MSM_CPU_FREQ_MIN;
+	policy->max = CONFIG_MSM_CPU_FREQ_MAX;
 #endif
 
 	if (is_clk)
